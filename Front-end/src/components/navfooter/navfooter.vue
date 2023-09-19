461c78657aa4178742389e793ea58d2276fc0612
luoyuanyuan <33892885+programmeryuanyuan@users.noreply.github.com>
2023-09-19 22:20:08 +1000

    Add files via upload

---
+++ Front-end/src/components/navfooter/navfooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="footer">
+    <span id="icp" class="fleft gray-link">
+      © 2023 -  MovieFun. All Rights Reserved.
+    </span>
+  </div>
+</template>
+
+<script>
+/**
+ * 顶部区域
+ */
+export default {
+  name: "navfooter",
+  setup(props, context) {
+
+  },
+};
+</script>
+
+<style scoped lang="less">
+#footer {
+  color: #999;
+  padding: 6px 0;
+  margin-top: 40px;
+  overflow: hidden;
+  zoom: 1;
+  border-top: 1px dashed #ddd;
+  text-align: center;
+}
+</style>
