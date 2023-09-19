9a5dbdb19f6d88ebd68c5d12863d4ed2b64ee506
luoyuanyuan <33892885+programmeryuanyuan@users.noreply.github.com>
2023-09-19 21:53:44 +1000

    Add files via upload

---
+++ Front-end/README.md
@@ -0,0 +1,27 @@
+
+## 安装依赖
+```
+npm install
+```
+
+### 开发模式启动（客户端渲染）
+```
+npm run dev:client
+```
+
+### 开发模式启动（服务端渲染）
+```
+npm run dev:ssr
+```
+
+### 生产模式打包
+```
+npm run build
+```
+
+### 生产模式预览
+```
+npm run serve
+```
+
+
---
+++ Front-end/vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import path from "path";
+
+export default defineConfig({
+  base: './', // 项目根路径
+  plugins: [vue()],// 配置vue插件
+  resolve: {
+    // 配置路径别名
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      'bootstrap': 'bootstrap/dist/js/bootstrap.min.js',
+      'bootstrap-css': 'bootstrap/dist/css/bootstrap.min.css',
+    },
+  },
+  server: {
+    port: 3001,
+    proxy: {
+      // 使用 proxy 实例
+      // 模拟请求豆瓣电影后台真实数据
+      '^/api': {
+        // target: 'https://frodo.douban.com',
+        target: 'https://api.themoviedb.org/3',
+        changeOrigin: true,
+        // 模拟referer和ua
+        headers: {
+          'referer': 'https://servicewechat.com/wx2f9b06c1de1ccfca/84/page-frame.html',
+          'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.16(0x18001042) NetType/WIFI Language/en_US'
+        }
+      }
+    }
+  }
+})
